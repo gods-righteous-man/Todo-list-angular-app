2b7062be41c8422318258ee918e69596b91c14ab
Gaurav Makasare <gauravmakasare@Gauravs-Air.lan>
2022-06-27 13:45:18 -0700

    adding all project files together

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {Task} from '../Task';
+import { Observable} from 'rxjs';
+import  { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders(
+    {
+      'Content-type' : 'application/json',
+    }
+  )
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = "http://localhost:5001/tasks";
+
+  constructor(private http:HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+
+  }
+
+  updateTaskReminder(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task:Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+}
